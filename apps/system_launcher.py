665d1b2235c1bc52ea142bc20dcf119ddfa63ced
VincTBest <vince.nemes2012@gmail.com>
2026-06-30 09:11:32 +0200

    Updated system_launcher.py

--- apps/system_launcher.py
+++ apps/system_launcher.py
@@ -1,6 +1,8 @@
+import apps.system as system
+
 def get_about():
-    return {"name": "System", "desc": "A basic app launcher and terminal.", "ver": "0.1.0", "hidden": False,
-    "author" : "Your Name",
+    return {"name": "System", "desc": "A basic app launcher and terminal.", "ver": system.get_about()["ver"], "hidden": False,
+    "author" : system.get_about()["author"],
     "upgrade_url": "https://raw.githubusercontent.com/VincTBest/BashOS/master/apps/system_launcher.py",} # The URL to your app. (for upgrades, set it to None if you don't have one.)
 
 def run(MODULES: dict, STORE: dict, SANDBOXED: bool, COMMANDS: dict):
